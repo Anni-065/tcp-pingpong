c6e187ff51ca51a6988b361d01b1a493058169fc
Athanasia Leontarakis <anni.leontarakis@gmail.com>
2023-10-13 21:22:42 +0900

    add multiple clients + 5 ping packets

--- src/client.py
+++ src/client.py
@@ -1,23 +1,17 @@
 import socket
-import time
 
 def start_client(host, port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
 
     while True:
-        message = "ping"
+        for i in range(5):
+            message = "ping"
+            client_socket.send(message.encode('utf-8'))
+            response = client_socket.recv(1024).decode('utf-8')
+            print(f"Received: {response}")
 
-        client_socket.send(message.encode('utf-8'))
-        
-        response = client_socket.recv(1024).decode('utf-8')
-        if not response:
-            break
-        print(f"Received: {response}")
-
-        time.sleep(2)
-
-    client_socket.close()
+        input("Press Enter to send 5 more pings")
 
 if __name__ == "__main__":
     host = "localhost"
--- src/server.py
+++ src/server.py
@@ -1,27 +1,40 @@
 import socket
+import threading
 
 def start_server(host, port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
-    server_socket.listen(1)
+    server_socket.listen()
 
     print(f"Server listening on {host}:{port}")
 
-    client_socket, client_address = server_socket.accept()
-    print(f"Connection from {client_address}")
+    connected_clients = []
 
     while True:
-        data = client_socket.recv(1024).decode('utf-8')
-        if not data:
-            break
-        print(f"Received: {data}")
-        
-        if data == "ping":
-            response = "pong"
-            client_socket.send(response.encode('utf-8'))
+        client_socket, client_address = server_socket.accept()
+        print(f"Connection from {client_address}")
 
-    client_socket.close()
-    server_socket.close()
+        connected_clients.append(client_socket)
+
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address, connected_clients))
+        client_thread.start()
+
+def handle_client(client_socket, client_address, connected_clients):
+    try:
+        while True:
+            data = client_socket.recv(1024).decode('utf-8')
+            if not data:
+                break
+            print(f"Received from {client_address}: {data}")
+            
+            if data == "ping":
+                response = "pong"
+                client_socket.send(response.encode('utf-8'))
+    except Exception as e:
+        print(f"Error handling client: {e}")
+    finally:
+        connected_clients.remove(client_socket)
+        client_socket.close()
 
 if __name__ == "__main__":
     host = "localhost"

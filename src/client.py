a3f0b18cf52d7c8713396dff756be55688d1c5d9
Athanasia Leontarakis <anni.leontarakis@gmail.com>
2023-10-13 22:08:18 +0900

    add RTT calculation

--- src/client.py
+++ src/client.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 def start_client(host, port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -7,9 +8,12 @@ def start_client(host, port):
     while True:
         for i in range(5):
             message = "ping"
+            start_time = time.time()
             client_socket.send(message.encode('utf-8'))
             response = client_socket.recv(1024).decode('utf-8')
-            print(f"Received: {response}")
+            end_time = time.time()
+            round_trip_time = (end_time - start_time) * 1000
+            print(f"Received: {response} (RRT: {round_trip_time:.3f}ms)")
 
         input("Press Enter to send 5 more pings")
 
